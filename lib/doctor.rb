a3ab1407ef7ebf4b33d330b0625cfe9e6b4628aa
Heidi Engstrom <hengstrom25@gmail.com>
2018-05-21 19:07:38 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,23 @@
+class Doctor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def appointments
+    self.appointment.collect { |songs| songs.appointment}
+  end  
+  
+end
